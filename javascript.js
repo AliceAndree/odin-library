ab06910d449671d3f8a788473de91818f57b342c
AliceAndree <thiel.alice@gmail.com>
2025-01-14 15:23:02 +0100

    Modal creation + css reorganisation

--- javascript.js
+++ javascript.js
@@ -1,28 +1,49 @@
+const addBookButton = document.querySelector("#add-book");
+const closeButton = document.querySelector("#close");
+const modal = document.querySelector("#modal");
+
+addBookButton.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
+
 const Book1 = new Book(
   "L'Amour sous Algorithme",
   "Judith Duportail",
   232,
-  "already read"
+  true,
+  "./assets/amour-sous-algo.jpeg"
 );
 
-const Book2 = new Book("La Volonté de Changer", "Bell Hooks", 226, "not read");
+const Book2 = new Book(
+  "Habiter En Oiseau",
+  "Vinciane Despret",
+  224,
+  false,
+  "./assets/habiter-en-oiseau.jpg"
+);
 
 const Book3 = new Book(
-  "Féminisme et Réseaux Sociaux",
-  "Elvire Duvelle-Charles",
-  212,
-  "not read"
+  "Le Plaisir",
+  "Maria Hesse",
+  160,
+  false,
+  "./assets/le-plaisir.jpg"
 );
 
 const myLibrary = [Book1, Book2, Book3];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, image) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.image = image;
   this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.read}, image: ${this.image}`;
   };
 }
 
